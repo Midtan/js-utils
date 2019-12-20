fbab19451e09db7980c370a766438fbac0e3141b
Midtan <Jonathan.Seltmann@gmx.net>
2019-12-20 16:09:45 +0100

    fixed reqursion in serializeArrayNotation

--- querytools.js
+++ querytools.js
@@ -1,6 +1,6 @@
 // Serializes to PHP array notation
 // Taken from https://stackoverflow.com/a/1714899/
-serializeArrayNotation = function(obj, prefix) {
+serializeArrayNotation = function (obj, prefix) {
   var str = [],
     p;
   for (p in obj) {
@@ -8,7 +8,7 @@ serializeArrayNotation = function(obj, prefix) {
       var k = prefix ? prefix + "[" + p + "]" : p,
         v = obj[p];
       str.push((v !== null && typeof v === "object") ?
-        serialize(v, k) :
+        serializeArrayNotation(v, k) :
         encodeURIComponent(k) + "=" + encodeURIComponent(v));
     }
   }
@@ -17,14 +17,14 @@ serializeArrayNotation = function(obj, prefix) {
 
 // Get the value of query parameters
 getParameter = function (parameterName) {
-	var href = location.href.replace(/\[\d*\]=/g, "=");
-	var url = new URL(href);
-	var params = url.searchParams.getAll(parameterName);
-	if (params.length > 1) {
-		return params;
-	} else if (params.length === 1) {
-		return params[0];
-	} else {
-		return null;
+  var href = location.href.replace(/\[\d*\]=/g, "=");
+  var url = new URL(href);
+  var params = url.searchParams.getAll(parameterName);
+  if (params.length > 1) {
+    return params;
+  } else if (params.length === 1) {
+    return params[0];
+  } else {
+    return null;
   }
 };
