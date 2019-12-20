97dad4a63ed582f925184ed31df5f50168129604
Midtan <Jonathan.Seltmann@gmx.net>
2019-12-20 13:36:27 +0100

    added serialize

---
+++ serialize.js
@@ -0,0 +1,15 @@
+// Taken from https://stackoverflow.com/a/1714899/
+serialize = function(obj, prefix) {
+  var str = [],
+    p;
+  for (p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      var k = prefix ? prefix + "[" + p + "]" : p,
+        v = obj[p];
+      str.push((v !== null && typeof v === "object") ?
+        serialize(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
+    }
+  }
+  return str.join("&");
+}
